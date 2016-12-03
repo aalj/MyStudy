54690e234fcae8b613c9c68265ba5bf9537e9a95
aalj <dqliangjun@126.com>
2016-12-03 17:32:26 +0800

    实现文件上传和下载

--- struts2/src/com/bestsnail/download/Download.java
+++ struts2/src/com/bestsnail/download/Download.java
@@ -1,16 +1,94 @@
 package com.bestsnail.download;
 
 import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.ServletActionContext;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
 
 /**
  * Created by stone on 16-12-1.
  */
-public class Download implements Action {
-    @Override
-    public String execute() throws Exception {
+public class Download extends ActionSupport {
 
+
+/**
+ * TODO: 16-12-3  能够实现下载
+ * 1、提供显示文件列表的方法
+ * 2、提供文件下的方法
+ */
+
+
+
+    String path = "/home/stone/workspace/DayNotes";
+    public String list() {
+
+
+        //1、获取要下载的文件路径下所有的文件列表
+
+        File files = new File(path);
+        String[] list = files.list();
+
+
+        //把得到的文件列表保存到request中
+        ActionContext context = ActionContext.getContext();
+        Map<String,Object> request = (Map<String, Object>) context.get("request");
+        request.put("filename",list);
+        for (String s : list) {
+            System.out.println(s);
+        }
+        return "list";
+    }
+
+    private String minghzi;
+
+    public void setMinghzi(String minghzi) {
+        System.out.println("转码之前的内容");
+        System.out.println("1==="+minghzi);
+        // 处理传入的参数中问题(get提交)
+//        try {
+//            minghzi = new String(minghzi.getBytes("ISO8859-1"),"UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            throw new RuntimeException(e);
+//        }
+        System.out.println("转码之后的内容");
+        System.out.println("2==="+minghzi);
+        // 把处理好的文件名，赋值
+        this.minghzi = minghzi;
+    }
+
+    public String downResule(){
 
         return "downloadResult";
     }
+
+    public InputStream getAttrInputStream(){
+        InputStream inputStream = null;
+        try{
+            File f = new File(path+"/"+minghzi);
+            inputStream = new FileInputStream(f);
+        }catch (Exception e){
+            new RuntimeException(e);
+        }
+        return inputStream;
+    }
+
+    // 4. 下载显示的文件名（浏览器显示的文件名）
+    public String getDownFileName() {
+        // 需要进行中文编码
+        try {
+            minghzi = URLEncoder.encode(minghzi, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        return minghzi;
+    }
+
+
 }
---
+++ struts2/web/WEB-INF/page/list.jsp
@@ -0,0 +1,44 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: stone
+  Date: 16-12-3
+  Time: 下午2:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>下载文件列表</title>
+</head>
+<body>
+
+<p>下载的文件列表</p>
+<table align="center" border="1">
+    <tr>
+        <td>标号</td>
+        <td>名字</td>
+        <td>点击下载</td>
+    </tr>
+    <c:forEach items="${filename}" varStatus="vs" var="filena" >
+        <tr>
+            <td>${vs.count}</td>
+            <td>${filena}</td>
+            <td>
+                <c:url var="url" value="download_downResule.action">
+                    <c:param name="minghzi" value="${filena}"></c:param>
+                </c:url>
+
+                <a href="${url}">下载</a>
+                
+                
+            </td>
+        </tr>
+
+
+    </c:forEach>
+
+</table>
+
+</body>
+</html>
